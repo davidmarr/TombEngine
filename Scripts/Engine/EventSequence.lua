503873d65c6c8d5f5be58a8ae9a05fd25ba2d059
davidmarr <116632612+davidmarr@users.noreply.github.com>
2025-05-31 14:34:09 +0200

    Update EventSequence.lua

--- Scripts/Engine/EventSequence.lua
+++ Scripts/Engine/EventSequence.lua
@@ -80,7 +80,7 @@ end
 -- @tparam string name A label to give the sequence; used to retrieve the timer later as well as internally by TEN.
 -- @tparam bool loop if true, the sequence will start again from its first timer once its final function has been called
 -- @tparam ?table|bool timerFormat Same as in <a href="Timer.html#timerFormat">Timer format</a> for Timer. This is mainly for debugging. __This will not work properly if another sequence or timer is showing a countdown.__
--- @tparam ?float|LevelFuncs|table ... A variable number of pairs of arguments, each pair consisting of:<br>
+-- @tparam float|LevelFuncs|table ... A variable number of pairs of arguments, each pair consisting of:<br>
 -- a time in seconds (positive values are accepted and with only 1 tenth of a second [__0.1__]),<br>
 -- followed by the function defined in the *LevelFuncs* table to call once the time has elapsed,<br>
 -- followed by another duration in seconds, another function name, etc.
@@ -130,7 +130,7 @@ end
 --    {LevelFuncs.Func2, "2", 5, 15},
 --    4.8,
 --    {LevelFuncs.Func2, "3", 5, 20})
-EventSequence.Create =function (name, loop, timerFormat, ...)
+EventSequence.Create = function (name, loop, timerFormat, ...)
 	if not Type.IsString(name) then
 		TEN.Util.PrintLog("Error in EventSequence.Create(): invalid name, sequence was not created", TEN.Util.LogLevel.ERROR)
 		return
