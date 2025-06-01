baa172302ddf82dcb359c9a78cf964a8065c3845
Lwmte <3331699+Lwmte@users.noreply.github.com>
2025-06-01 17:09:25 +0200

    Fixed lasers not working

--- TombEngine/Objects/TR5/Trap/LaserBarrier.cpp
+++ TombEngine/Objects/TR5/Trap/LaserBarrier.cpp
@@ -91,8 +91,6 @@ namespace TEN::Entities::Traps
 		float barrierHeight = item.Pose.Position.y - pointColl.GetCeilingHeight();
 		item.ItemFlags[0] = barrierHeight;
 
-		item.Collidable = false;
-
 		// Initialize barrier effect.
 		auto barrier = LaserBarrier{};
 		barrier.Initialize(item);
