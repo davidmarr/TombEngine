0f72008b3b090ae9116da6ead5fc0492e9f54a20
Lwmte <3331699+Lwmte@users.noreply.github.com>
2025-10-28 00:56:08 +0100

    Fixed HUD elements display in flybys and title

--- TombEngine/Game/Hud/Hud.cpp
+++ TombEngine/Game/Hud/Hud.cpp
@@ -1,6 +1,8 @@
 #include "framework.h"
 #include "Game/Hud/Hud.h"
 
+#include "Game/camera.h"
+#include "Game/control/control.h"
 #include "Game/Hud/PickupSummary.h"
 #include "Game/Hud/StatusBars.h"
 #include "Game/items.h"
@@ -11,17 +13,30 @@ namespace TEN::Hud
 {
 	HudController g_Hud = {};
 
+	bool HudController::CanDrawHud() const
+	{
+		// Avoid drawing in title level and during cutscenes.
+		return (CurrentLevel != 0 && CinematicBarsHeight <= EPSILON);
+	}
+
 	void HudController::Update(const ItemInfo& playerItem)
 	{
+		// Delay pickup summary update until HUD is available again to avoid
+		// lack of consequential indication, e.g. after flyby triggered by a pickup.
+		if (CanDrawHud())
+			PickupSummary.Update();
+
 		InteractionHighlighter.Update();
 		TargetHighlighter.Update(playerItem);
 		Speedometer.Update();
-		PickupSummary.Update();
 		StatusBars.Update(playerItem);
 	}
 
 	void HudController::Draw2D(const ItemInfo& playerItem) const
 	{
+		if (!CanDrawHud())
+			return;
+
 		InteractionHighlighter.Draw();
 		TargetHighlighter.Draw();
 		Speedometer.Draw();
@@ -30,6 +45,9 @@ namespace TEN::Hud
 
 	void HudController::Draw3D()
 	{
+		if (!CanDrawHud())
+			return;
+
 		PickupSummary.Draw();
 	}
 
