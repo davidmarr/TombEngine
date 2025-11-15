3ee5a78d6b9a2d1f4e46566aeeffa7b006fcdea4
davidmarr <116632612+davidmarr@users.noreply.github.com>
2025-11-15 11:43:10 +0100

    Update Util.lua

--- Scripts/Engine/Util.lua
+++ Scripts/Engine/Util.lua
@@ -1,16 +1,15 @@
 -- ldignore
 local Util = {}
 local Type = require("Engine.Type")
-
+local validKeys = { hours = true, minutes = true, seconds = true, deciseconds = true }
 Util.ShortenTENCalls = function()
 	print("Util.ShortenTENCalls is deprecated; its functionality is now performed automatically by TombEngine.")
 end
 
 -- Check if the time format is correct.
-Util.CheckTimeFormat = function (timerFormat, errorText)
+Util.CheckTimeFormat = function(timerFormat, errorText)
 	errorText = errorText and Type.IsString(errorText) and errorText or false
 	if Type.IsTable(timerFormat) then
-		local validKeys = {hours = true, minutes = true, seconds = true, deciseconds = true}
 		for k, v in pairs(timerFormat) do
 			if not validKeys[k] or type(v) ~= "boolean" then
 				if errorText then
@@ -21,75 +20,69 @@ Util.CheckTimeFormat = function (timerFormat, errorText)
 		end
 		return timerFormat
 	elseif Type.IsBoolean(timerFormat) then
-		return timerFormat and {seconds = true} or timerFormat
+		return timerFormat and { seconds = true } or timerFormat
 	end
-    if errorText then
-        TEN.Util.PrintLog(errorText, TEN.Util.LogLevel.WARNING)
-    end
+	if errorText then
+		TEN.Util.PrintLog(errorText, TEN.Util.LogLevel.WARNING)
+	end
 	return false
 end
 
 -- Generate a formatted string from a time.
-Util.GenerateTimeFormattedString = function (time, timerFormat, errorFormat)
-    errorFormat = Type.IsString(errorFormat) and errorFormat or false
-    timerFormat = Util.CheckTimeFormat(timerFormat)
+Util.GenerateTimeFormattedString = function(time, timerFormat, errorFormat, check)
+	check = (check == nil or not Type.IsBoolean(check)) and true or check
+	errorFormat = Type.IsString(errorFormat) and errorFormat or false
+	timerFormat = check and Util.CheckTimeFormat(timerFormat, errorFormat) or timerFormat
 
-	if not timerFormat then
-		if errorFormat then
-			TEN.Util.PrintLog(errorFormat, TEN.Util.LogLevel.ERROR)
-		end
-		return "Error"
-	else
-		local result = {}
-		local index = 1
-		if timerFormat.hours then
-        	result[index] = string.format("%02d", time.h)
-        	index = index + 1
-    	end
-    	if timerFormat.minutes then
-        	result[index] = string.format("%02d", timerFormat.hours and time.m or (time.m + (60 * time.h)))
-        	index = index + 1
-    	end
-    	if timerFormat.seconds then
-        	result[index] = string.format("%02d", timerFormat.minutes and time.s or (time.s + (60 * time.m)))
-        	index = index + 1
-    	end
-		local formattedString = table.concat(result, ":")
-
-    	if timerFormat.deciseconds then
-        	local deciseconds = math.floor(time.c / 10)
-			return (index == 1) and deciseconds or formattedString .. "." .. deciseconds
-    	end
-    	return formattedString
+	local result = {}
+	local index = 1
+	if timerFormat.hours then
+		result[index] = string.format("%02d", time.h)
+		index = index + 1
 	end
+	if timerFormat.minutes then
+		result[index] = string.format("%02d", timerFormat.hours and time.m or (time.m + (60 * time.h)))
+		index = index + 1
+	end
+	if timerFormat.seconds then
+		result[index] = string.format("%02d", timerFormat.minutes and time.s or (time.s + (60 * time.m)))
+		index = index + 1
+	end
+	local formattedString = table.concat(result, ":")
+
+	if timerFormat.deciseconds then
+		local deciseconds = math.floor(time.c / 10)
+		return (index == 1) and tostring(deciseconds) or formattedString .. "." .. deciseconds
+	end
+	return formattedString
 end
 
 -- Check if table has particular value.
-Util.TableHasValue = function (tbl, val)
-    if not Type.IsTable(tbl) then
-        return false
-    end
-    for _, value in pairs(tbl) do
-        if value == val then
-            return true
-        end
-    end
-    return false
+Util.TableHasValue = function(tbl, val)
+	if not Type.IsTable(tbl) then
+		return false
+	end
+	for _, value in pairs(tbl) do
+		if value == val then
+			return true
+		end
+	end
+	return false
 end
 
 -- Compare two values.
 local operators = {
-    function(a, b) return a == b end,
-    function(a, b) return a ~= b end,
-    function(a, b) return a < b end,
-    function(a, b) return a <= b end,
-    function(a, b) return a > b end,
-    function(a, b) return a >= b end,
+	function(a, b) return a == b end,
+	function(a, b) return a ~= b end,
+	function(a, b) return a < b end,
+	function(a, b) return a <= b end,
+	function(a, b) return a > b end,
+	function(a, b) return a >= b end,
 }
 Util.CompareValue = function(operand, reference, operator)
-    operand = operand == true and 1 or operand == false and 0 or operand
-    reference = reference == true and 1 or reference == false and 0 or reference
-    return operators[operator + 1] and operators[operator + 1](operand, reference) or false
+	operand = operand == true and 1 or operand == false and 0 or operand
+	reference = reference == true and 1 or reference == false and 0 or reference
+	return operators[operator + 1] and operators[operator + 1](operand, reference) or false
 end
 
 return Util
