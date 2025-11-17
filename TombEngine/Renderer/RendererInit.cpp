c737b1579adfb8124b16cbd08ebddd24a6fbe018
Lwmte <3331699+Lwmte@users.noreply.github.com>
2025-11-17 23:26:03 +0100

    Fixed exclude blend mode again

--- TombEngine/Renderer/RendererInit.cpp
+++ TombEngine/Renderer/RendererInit.cpp
@@ -136,7 +136,7 @@ namespace TEN::Renderer
 		blendStateDesc.RenderTarget[0].BlendEnable = true;
 		blendStateDesc.RenderTarget[0].SrcBlend = D3D11_BLEND_INV_DEST_COLOR;
 		blendStateDesc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_COLOR;
-		blendStateDesc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+		blendStateDesc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_SUBTRACT;
 		blendStateDesc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
 		blendStateDesc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;
 		blendStateDesc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
