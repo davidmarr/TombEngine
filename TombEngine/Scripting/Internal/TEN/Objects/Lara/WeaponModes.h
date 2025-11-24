e172238ef8984e9636ab726c2b40d8a585917676
TrainWrack <120750885+TrainWrack@users.noreply.github.com>
2025-11-24 09:08:02 -0500

    Address WeaponMode comments

--- TombEngine/Scripting/Internal/TEN/Objects/Lara/WeaponModes.h
+++ TombEngine/Scripting/Internal/TEN/Objects/Lara/WeaponModes.h
@@ -3,16 +3,16 @@
 #include "Game/Lara/lara_struct.h"
 
 
-/// Constants for HK Weapon modes.
+/// Constants for Weapon modes.
 // @enum Objects.WeaponMode
 // @pragma nostrip
 
 /// Table of Objects.WeaponMode constants.
-// To be used with @{Objects.LaraObject.SetWeaponMode} function.
+// To be used with @{Objects.LaraObject.SetWeaponMode} function. Currently only works for HK Weapon.
 //
-// - `HK_RAPID_MODE` Sets the HK Rapid mode.
-// - `HK_BURST_MODE` Sets the HK Burst mode.
-// - `HK_SNIPER_MODE` Sets the HK Sniper mode.
+// - `RAPID_MODE` Sets the Rapid mode.
+// - `BURST_MODE` Sets the Burst mode.
+// - `SNIPER_MODE` Sets the Sniper mode.
 //
 // @table Objects.WeaponMode
 namespace TEN::Scripting
@@ -20,8 +20,8 @@ namespace TEN::Scripting
 
 	static const auto WEAPON_MODES = std::unordered_map<std::string, PlayerWeaponMode>
 	{
-		{ "HK_RAPID_MODE", PlayerWeaponMode::HKRapid },
-		{ "HK_BURST_MODE", PlayerWeaponMode::HKBurst },
-		{ "HK_SNIPER_MODE", PlayerWeaponMode::HKSniper }
+		{ "RAPID_MODE", PlayerWeaponMode::HKRapid },
+		{ "BURST_MODE", PlayerWeaponMode::HKBurst },
+		{ "SNIPER_MODE", PlayerWeaponMode::HKSniper }
 	};
 }
