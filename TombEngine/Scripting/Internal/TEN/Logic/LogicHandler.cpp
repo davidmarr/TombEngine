7d638fe7b676d9c28e00ed6955aef8bc1556c95d
davidmarr <116632612+davidmarr@users.noreply.github.com>
2025-11-25 20:19:40 +0100

    Fix ShortenTENCalls function (#1797)
    * Exclude 'DisplaySprite' from global assignment in ShortenTENCalls
    
    Added an exceptions table to prevent 'DisplaySprite' from being assigned to the global environment in the ShortenTENCalls function. This avoids potential conflicts or warnings for this specific key.
    
    * Update LogicHandler.cpp

--- TombEngine/Scripting/Internal/TEN/Logic/LogicHandler.cpp
+++ TombEngine/Scripting/Internal/TEN/Logic/LogicHandler.cpp
@@ -978,10 +978,13 @@ void LogicHandler::ResetVariables()
 void LogicHandler::ShortenTENCalls()
 {
 	auto str = R"(local ShortenInner 
+	local exceptions = {
+		DisplaySprite = true,
+	}
 
 	ShortenInner = function(tab)
 		for k, v in pairs(tab) do
-			if _G[k] then
+			if _G[k] and not exceptions[k] then
 				print("WARNING! Key " .. k .. " already exists in global environment!")
 			else
 				_G[k] = v
