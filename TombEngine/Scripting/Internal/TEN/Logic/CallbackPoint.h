7feaebf0bafa349af1081ac8644981d159f0121e
davidmarr <116632612+davidmarr@users.noreply.github.com>
2025-11-29 15:42:01 +0100

    Fix comment formatting in CallbackPoint.h

--- TombEngine/Scripting/Internal/TEN/Logic/CallbackPoint.h
+++ TombEngine/Scripting/Internal/TEN/Logic/CallbackPoint.h
@@ -49,11 +49,11 @@ namespace TEN::Scripting
 		// @mem POST_END
 		{ ScriptReserved_PostEnd, CallbackPoint::PostEnd },
 
-		/// Will be called immediately before LevelFuncs.OnUseItem. This accepts functions that take an objectNumber argument, such as OnUseItem.
+		/// Will be called immediately before LevelFuncs.OnUseItem.
 		// @mem PRE_USE_ITEM
 		{ ScriptReserved_PreUseItem, CallbackPoint::PreUseItem },
 
-		/// Will be called immediately after LevelFuncs.OnUseItem. This accepts functions that take an objectNumber argument, such as OnUseItem.
+		/// Will be called immediately after LevelFuncs.OnUseItem.
 		// @mem POST_USE_ITEM
 		{ ScriptReserved_PostUseItem, CallbackPoint::PostUseItem },
 
