dd4be6e3b47a88743c677dc21381ba404bb0286e
MontyTRC89 <montyhammet@hotmail.it>
2026-01-09 11:06:41 +0100

    Removed bad code in horse init

--- TombEngine/Objects/TR4/tr4_objects.cpp
+++ TombEngine/Objects/TR4/tr4_objects.cpp
@@ -499,10 +499,6 @@ namespace TEN::Entities
 			obj->collision = ObjectCollision;
 			obj->control = nullptr;
 			obj->SetHitEffect(true);
-			// NOTE: Set intelligent to prevent horse from blocking LOS to horseman.
-			// Set NOT_TARGETABLE to ensure horse is never a valid target.
-			obj->intelligent = true;
-			obj->HitPoints = NOT_TARGETABLE;
 		}
 
 		obj = &Objects[ID_HORSEMAN];
