65957c2274921bdac5deb28901590491420dc9f2
MontyTRC89 <montyhammet@hotmail.it>
2026-01-09 11:06:11 +0100

    Revert "Fixed horse and horseman interaction"
    This reverts commit 0d528e1f4235c09a4a4a487c76668636c63e7bed.

--- TombEngine/Objects/TR4/tr4_objects.cpp
+++ TombEngine/Objects/TR4/tr4_objects.cpp
@@ -499,8 +499,10 @@ namespace TEN::Entities
 			obj->collision = ObjectCollision;
 			obj->control = nullptr;
 			obj->SetHitEffect(true);
-			obj->IgnoreInLOSCheck = true;
-
+			// NOTE: Set intelligent to prevent horse from blocking LOS to horseman.
+			// Set NOT_TARGETABLE to ensure horse is never a valid target.
+			obj->intelligent = true;
+			obj->HitPoints = NOT_TARGETABLE;
 		}
 
 		obj = &Objects[ID_HORSEMAN];
