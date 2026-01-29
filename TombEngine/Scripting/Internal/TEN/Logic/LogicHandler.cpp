6f1fa5e0ff5261648b5ba26d28c02172c7fa2a6b
davidmarr <116632612+davidmarr@users.noreply.github.com>
2026-01-29 13:24:37 +0100

    Clarify note on callback execution order in docs
    Updated documentation to clarify that the order of execution for functions registered with the same CallbackPoint is undefined. Reformatted the note for better readability.

--- TombEngine/Scripting/Internal/TEN/Logic/LogicHandler.cpp
+++ TombEngine/Scripting/Internal/TEN/Logic/LogicHandler.cpp
@@ -157,7 +157,7 @@ This is intended for module/library developers who want their modules to do
 stuff during level start/load/end/save/control phase, but don't want the level
 designer to add calls to `OnStart`, `OnLoad`, etc. in their level script.
 
-__Please note__: The order in which two functions with the same CallbackPoint are called is undefined.
+Note: __the order in which two functions with the same CallbackPoint are called is undefined__.<br>
 i.e. if you register `MyFunc` and `MyFunc2` with `PRE_LOOP`, both will be called in the beginning of game loop, but there is no guarantee that `MyFunc` will be called before `MyFunc2`, or vice-versa.
 
 Any returned value will be discarded.
