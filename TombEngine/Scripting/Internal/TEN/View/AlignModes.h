91edf4891e942cfc809be3f1d033b93c27c04794
davidmarr <116632612+davidmarr@users.noreply.github.com>
2026-02-01 13:59:41 +0100

    Reorder and update sprite alignment modes
    Rearranged the ALIGN_MODES map to improve logical grouping and updated comments for clarity. Center, top, and bottom alignments are now listed in a more intuitive order.

--- TombEngine/Scripting/Internal/TEN/View/AlignModes.h
+++ TombEngine/Scripting/Internal/TEN/View/AlignModes.h
@@ -13,37 +13,38 @@ namespace TEN::Scripting::View
 
 	static const std::unordered_map<std::string, DisplaySpriteAlignMode> ALIGN_MODES
 	{
-		/// Center alignment.
-		// @mem CENTER
-		{ "CENTER", DisplaySpriteAlignMode::Center },
-
-		/// Center top alignment.
-		// @mem CENTER_TOP
-		{ "TOP_CENTER", DisplaySpriteAlignMode::CenterTop },
-
-		/// Center bottom alignment.
-		// @mem CENTER_BOTTOM
-		{ "BOTTOM_CENTER", DisplaySpriteAlignMode::CenterBottom },
-
-		/// Center left alignment.
-		// @mem CENTER_LEFT
-		{ "CENTER_LEFT", DisplaySpriteAlignMode::CenterLeft },
-
-		/// Center right alignment.
-		// @mem CENTER_RIGHT
-		{ "CENTER_RIGHT", DisplaySpriteAlignMode::CenterRight },
 
 		/// Top left alignment.
 		// @mem TOP_LEFT
 		{ "TOP_LEFT", DisplaySpriteAlignMode::TopLeft },
 
+		/// Center top alignment.
+		// @mem TOP_CENTER
+		{ "TOP_CENTER", DisplaySpriteAlignMode::CenterTop },
+
 		/// Top right alignment.
 		// @mem TOP_RIGHT
 		{ "TOP_RIGHT", DisplaySpriteAlignMode::TopRight },
 
+		/// Center left alignment.
+		// @mem CENTER_LEFT
+		{ "CENTER_LEFT", DisplaySpriteAlignMode::CenterLeft },
+
+		/// Center alignment.
+		// @mem CENTER
+		{ "CENTER", DisplaySpriteAlignMode::Center },
+
+		/// Center right alignment.
+		// @mem CENTER_RIGHT
+		{ "CENTER_RIGHT", DisplaySpriteAlignMode::CenterRight },
+
 		/// Bottom left alignment.
 		// @mem BOTTOM_LEFT
 		{ "BOTTOM_LEFT", DisplaySpriteAlignMode::BottomLeft },
+
+		/// Center bottom alignment.
+		// @mem BOTTOM_CENTER
+		{ "BOTTOM_CENTER", DisplaySpriteAlignMode::CenterBottom },
 
 		/// Bottom right alignment.
 		// @mem BOTTOM_RIGHT
