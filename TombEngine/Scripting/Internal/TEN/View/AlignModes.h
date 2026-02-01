4ba9d83be83c5f25459009578202ec00384cbd79
davidmarr <116632612+davidmarr@users.noreply.github.com>
2026-02-01 12:28:19 +0100

    Update alignment mode keys and add compatibility aliases
    Reordered and renamed alignment mode keys for clarity (e.g., 'CENTER_TOP' to 'TOP_CENTER', 'CENTER_BOTTOM' to 'BOTTOM_CENTER'). Added compatibility aliases for 'CENTER_TOP' and 'CENTER_BOTTOM' to maintain support for legacy code.

--- TombEngine/Scripting/Internal/TEN/View/AlignModes.h
+++ TombEngine/Scripting/Internal/TEN/View/AlignModes.h
@@ -13,40 +13,44 @@ namespace TEN::Scripting::View
 
 	static const std::unordered_map<std::string, DisplaySpriteAlignMode> ALIGN_MODES
 	{
-		/// Center alignment.
-		// @mem CENTER
-		{ "CENTER", DisplaySpriteAlignMode::Center },
-
-		/// Center top alignment.
-		// @mem CENTER_TOP
-		{ "CENTER_TOP", DisplaySpriteAlignMode::CenterTop },
-
-		/// Center bottom alignment.
-		// @mem CENTER_BOTTOM
-		{ "CENTER_BOTTOM", DisplaySpriteAlignMode::CenterBottom },
-
-		/// Center left alignment.
-		// @mem CENTER_LEFT
-		{ "CENTER_LEFT", DisplaySpriteAlignMode::CenterLeft },
-
-		/// Center right alignment.
-		// @mem CENTER_RIGHT
-		{ "CENTER_RIGHT", DisplaySpriteAlignMode::CenterRight },
-
 		/// Top left alignment.
 		// @mem TOP_LEFT
 		{ "TOP_LEFT", DisplaySpriteAlignMode::TopLeft },
+
+		/// Center top alignment.
+		// @mem CENTER_TOP
+		{ "TOP_CENTER", DisplaySpriteAlignMode::CenterTop },
 
 		/// Top right alignment.
 		// @mem TOP_RIGHT
 		{ "TOP_RIGHT", DisplaySpriteAlignMode::TopRight },
 
+		/// Center left alignment.
+		// @mem CENTER_LEFT
+		{ "CENTER_LEFT", DisplaySpriteAlignMode::CenterLeft },
+
+		/// Center alignment.
+		// @mem CENTER
+		{ "CENTER", DisplaySpriteAlignMode::Center },
+
+		/// Center right alignment.
+		// @mem CENTER_RIGHT
+		{ "CENTER_RIGHT", DisplaySpriteAlignMode::CenterRight },
+
 		/// Bottom left alignment.
 		// @mem BOTTOM_LEFT
 		{ "BOTTOM_LEFT", DisplaySpriteAlignMode::BottomLeft },
 
+		/// Center bottom alignment.
+		// @mem CENTER_BOTTOM
+		{ "BOTTOM_CENTER", DisplaySpriteAlignMode::CenterBottom },
+
 		/// Bottom right alignment.
 		// @mem BOTTOM_RIGHT
-		{ "BOTTOM_RIGHT", DisplaySpriteAlignMode::BottomRight }
+		{ "BOTTOM_RIGHT", DisplaySpriteAlignMode::BottomRight },
+
+		// COMPATIBILITY
+		{ "CENTER_BOTTOM", DisplaySpriteAlignMode::CenterBottom },
+		{ "CENTER_TOP", DisplaySpriteAlignMode::CenterTop },
 	};
 }
