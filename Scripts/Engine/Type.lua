d97e3a5d02e08d2f565f35b856b20dc30eea8ce0
davidmarr <116632612+davidmarr@users.noreply.github.com>
2026-09-12 16:16:20 +0200

    Fix typo in module description comment

--- Scripts/Engine/Type.lua
+++ Scripts/Engine/Type.lua
@@ -1,5 +1,5 @@
 -----<style>table.function_list td.name {min-width: 335px;}</style>
---- This molule contains functions that allow to check the data type of a variable. It also contains functions that allow to check if the variable is a TEN primitive class or a LevelFuncs.
+--- This module contains functions that allow to check the data type of a variable. It also contains functions that allow to check if the variable is a TEN primitive class or a LevelFuncs.
 --
 --
 -- To use the functions within the scripts, the module must be called:
